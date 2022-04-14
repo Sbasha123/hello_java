37bbb9db253d7b044a24c5174f172c1ba54a9225
basha <basha516321@gmail.com>
2022-04-14 07:39:56 +0000

    added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("aman sara");
-    System.out.println("aman sadhiya");
+    System.out.println("sara");
+    System.out.println("sadhiya");
   }
 }
