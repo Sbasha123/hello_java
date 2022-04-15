2c8abce4881ded078137c44f8724c356fb3a3f64
basha <basha516321@gmail.com>
2022-04-15 11:48:57 +0000

    added new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("sara");
     System.out.println("sadhiya");
+    System.out.println("both are sisters");
   }
 }
